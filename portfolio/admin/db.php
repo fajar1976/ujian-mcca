421ed119ea461151b70533dc047d7cb68c9baab9
Fajar Pahmi Padilah <fajarpahmi.2021@student.uny.ac.id>
2023-05-26 17:02:08 +0700

    yuk bisa

--- portfolio/admin/db.php
+++ portfolio/admin/db.php
@@ -1,8 +1,8 @@
 <?php 
 
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
+const DB_HOST = '4.194.83.136';
+const DB_USER = 'admin';
+const DB_PASSWORD = 'root';
 const DB_NAME     = 'personal_portfolio';
 
 $dbcon = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
