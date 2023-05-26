5a2f80b26800d55b62d2a82c0d7d2e4df627c47a
Fajar Pahmi Padilah <fajarpahmi.2021@student.uny.ac.id>
2023-05-26 18:00:26 +0700

    lah jadi error

--- portfolio/admin/db.php
+++ portfolio/admin/db.php
@@ -2,7 +2,7 @@
 
 const DB_HOST = 'ujianmcca';
 const DB_USER = 'admin';
-const DB_PASSWORD = 'root';
+const DB_PASSWORD = 'ujianmcca';
 const DB_NAME     = 'personal_portfolio';
 
 $dbcon = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
